3006eef9c912a01de1c0edb4d4377ac31dea25b6
Nadir <2266923@bdeb.qc.ca>
2023-01-20 17:57:22 -0500

    establishing a connection to a socket server

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,27 +3,35 @@ import Header from "../../components/header";
 import Message from "../../components/message";
 import styles from "../styles/index.module.css";
 import Chatbox from "../../components/chatbox";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+let socket;
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+	useEffect(() => {
+		socketInitializer();
+	}, []);
+
+	const socketInitializer = async () => {
+		await fetch("/api/socketio");
+		if (!socket) {
+			socket = io();
+
+			socket.on("test", () => {
+				console.log("test");
+			});
+		}
+	};
+
 	return (
 		<>
 			<Header />
 			<div className={styles.messagesContainer}>
 				<Message
-					authorName='bob'
-					message='This is a message example !! Did you know that i like this app. crazy ik. random stuff'
-					byAuthUser={false}
-				/>
-				<Message
-					authorName='bob'
-					message='This is a message example !! Did you know that i like this app. crazy ik. random stuff'
-					byAuthUser={true}
-				/>
-				<Message
-					authorName='bob'
-					message='This is a message example !! Did you know that i like this app. crazy ik. random stuff'
+					authorName='Doschat developer'
+					message='Welcome to Doschat! You can start chatting right away!'
 					byAuthUser={false}
 				/>
 			</div>
